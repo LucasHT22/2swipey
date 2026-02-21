b16557473f1691d1a5babc136d3aa8386ad6a158
Lucas Honda <88567622+LucasHT22@users.noreply.github.com>
2026-02-21 08:47:09 -0800

    add timer, missing time checker

--- scripts/Panel.gd
+++ scripts/Panel.gd
@@ -1,5 +1,11 @@
 extends Panel
 
+@onready var time_bar: ProgressBar = $ProgressBar
+@onready var timer: Timer = $Timer
+
+const TIME_BONUS: float = 25.0
+const TIME_LOST: float = 2.0
+
 var clues = [
 	"veggie bowl",
 	"island money",
@@ -23,23 +29,36 @@ var current_index : int
 func get_clue():
 	current_index = randi() % clues.size()
 	clue_label.text = clues[current_index]
-	print(results[current_index])
 
 func check_answer():
 	var ip_card = Global.input_card
 	
 	if ip_card.contains(results[current_index]):
 		Global.progress += 10
-		print("Correct:", Global.progress)
+		add_time_bonus()
+		print("Correct:", Global.progress, "Timer:", timer.time_left)
 	else:
 		Global.progress -= 10
+		remove_time_bonus()
 		print("Wrong:", Global.progress)
-		if Global.progress <= 0:
+		if Global.progress <= 0 || timer.timeout:
 			get_tree().change_scene_to_file("res://scenes/game_over.tscn")
 	
 	get_clue()
 
+func add_time_bonus():
+	time_bar.value += TIME_BONUS
+	time_bar.value = clamp(time_bar.value, time_bar.min_value, time_bar.max_value)
+	
+func remove_time_bonus():
+	time_bar.value -= TIME_LOST
+	time_bar.value = clamp(time_bar.value, time_bar.min_value, time_bar.max_value)
 
 func _ready():
+	time_bar.value = time_bar.max_value
 	randomize()
 	get_clue()
+
+func check_time():
+	if timer.timeout:
+		get_tree().change_scene_to_file("res://scenes/game_over.tscn")
