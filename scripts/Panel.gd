d84e9ca778218f7b5ba373fc23ef1b9cc3f399e4
MeghanaM4 <meghanambpd@gmail.com>
2026-02-21 07:30:11 -0800

    fix validator

--- scripts/Panel.gd
+++ scripts/Panel.gd
@@ -4,24 +4,26 @@ var clues = [
 	"veggie bowl",
 	"island money",
 	"big red ball",
-	"buy in bulk"
+	"buy in bulk",
+	"strrrrrrrrrrrrrrrrrrrike!"
 ]
 
 var results = [
-	"%B6010563596189797¨CHIPOTLE",
-	"%B4859530006682470ËGAN;SOFIA",
+	"%B6010563596189797^CHIPOTLE",
+	"%B4859530006682470^EGAN/SOFIA",
 	"",
-	"%7001112034796255¨BOORGU;MANITEJ"
+	"%7001112034796255^BOORGU/MANITEJ",
+	"%6686=6673103695066313650?"
 ]
 
 var current_index : int
 
 @onready var clue_label = $ClueLabel
 
-
 func get_clue():
 	current_index = randi() % clues.size()
 	clue_label.text = clues[current_index]
+	print(results[current_index])
 
 func check_answer():
 	var ip_card = Global.input_card
--- scripts/line_edit.gd
+++ scripts/line_edit.gd
@@ -4,7 +4,9 @@ func _ready():
 	grab_focus()
 
 func _on_text_submitted(new_text: String) -> void:
-	Global.swipe = true
 	Global.input_card = new_text
 	print(Global.input_card)
+	
+	var panel = get_node("/root/TipsMenu/Background/MarginContainer/Panel")
+	panel.check_answer()
 	clear()
