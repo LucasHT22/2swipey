a627d7f78dbb7e09f8d111f9bb47501bafcbe236
MeghanaM4 <meghanambpd@gmail.com>
2026-02-21 21:41:17 -0800

    Revert "bgs"
    This reverts commit e66c8c0d20959b721a79cd975400a8d4673dbba8.

--- scripts/camera_2d.gd
+++ scripts/camera_2d.gd
@@ -4,9 +4,7 @@ extends Camera2D
 @export var heidi: CharacterBody2D
 const CAMERA_SPEED = 5.0 # Adjust the speed of the camera movement
 
-func _ready():
-	self.limit_top = -450
-	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float):
 	if orpheus and heidi:
 		var midpoint = (orpheus.global_position + heidi.global_position) / 2.0
