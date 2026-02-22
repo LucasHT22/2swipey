35671ef404b1cf6cb5997fa722684c678ed64286
MeghanaM4 <meghanambpd@gmail.com>
2026-02-22 08:35:07 -0800

    shake safe

--- scripts/Panel.gd
+++ scripts/Panel.gd
@@ -6,8 +6,9 @@ extends Panel
 @onready var feedback_decline: Sprite2D = $Decline
 @onready var feedback_accept_sound: AudioStreamPlayer = $Accepted_sound
 @onready var feedback_decline_sound: AudioStreamPlayer = $Declined_sound
+@onready var safe_sprite: AnimatedSprite2D = $safe
 
-const TIME_BONUS: float = 10.0
+const TIME_BONUS: float = 20.0
 const TIME_LOST: float = 5.0
 
 var clues = [
@@ -53,11 +54,13 @@ func check_answer():
 		feedback_accept.visible = false
 		print("Correct:", Global.progress)
 		print(timer.time_left)
+		
 	else:
 		Global.progress -= 10
 		remove_time_bonus()
 		feedback_decline.visible = true
 		feedback_decline_sound.play()
+		shake_safe()
 		await get_tree().create_timer(1.0).timeout
 		feedback_decline.visible = false
 		print(timer.time_left)
@@ -88,3 +91,12 @@ func check_time():
 
 func _on_timer_timeout() -> void:
 	get_tree().change_scene_to_file("res://scenes/level2.tscn")
+	
+func shake_safe():
+	var original_pos = safe_sprite.position
+	var tween = create_tween()
+	tween.tween_property(safe_sprite, "position", original_pos + Vector2(10, 0), 0.05)
+	tween.tween_property(safe_sprite, "position", original_pos + Vector2(-10, 0), 0.05)
+	tween.tween_property(safe_sprite, "position", original_pos + Vector2(8, 0), 0.05)
+	tween.tween_property(safe_sprite, "position", original_pos + Vector2(-8, 0), 0.05)
+	tween.tween_property(safe_sprite, "position", original_pos, 0.05)
